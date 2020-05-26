81414745c2720232a74b573c70459fa950a5d6b7
csosa27 <eng.csosa.92@gmail.com>
2020-05-26 17:44:47 -0400

    Feat: adding navigation and stuff

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PlacesNavigator from './navigation/PlacesNavigator';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
+import placesReducer from './store/places-reducer';
 
 export default function App() {
+  const rootReducer = combineReducers({
+    places: placesReducer
+  });
+
+  const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
