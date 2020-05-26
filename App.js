955d6c6d39217c902992e196e30193aaaea423e1
csosa27 <eng.csosa.92@gmail.com>
2020-05-26 19:41:34 -0400

    Feat: database access with sqlite

--- App.js
+++ App.js
@@ -7,13 +7,22 @@ import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
 import placesReducer from './store/places-reducer';
+import { init } from './helpers/db';
+
+init().then(() => {
+  console.log('Initialize database');
+}).catch(err => {
+  console.log('Initializing db failed.');
+  console.log(err);
+});
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
-  const rootReducer = combineReducers({
-    places: placesReducer
-  });
-
-  const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
   return (
     <Provider store={store}>
