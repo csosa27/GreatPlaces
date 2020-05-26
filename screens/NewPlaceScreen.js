e52098c6fbaa23151c4eadf584a5a2010dad2d03
csosa27 <eng.csosa.92@gmail.com>
2020-05-26 18:45:49 -0400

    feat: added camera access

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -4,9 +4,11 @@ import { useDispatch } from 'react-redux';
 
 import Colors from '../constants/Colors';
 import * as placesActions from '../store/places-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const NewPlaceScreen = props => {
     const [titleValue, setTitleValue] = useState('');
+    const [selectedImage, setSelectedImage] = useState();
 
     const dispatch = useDispatch();
 
@@ -14,8 +16,12 @@ const NewPlaceScreen = props => {
         setTitleValue(text);
     };
 
+    const imageTakenHandler = imagePath => {
+        setSelectedImage(imagePath)
+    };
+
     const savePlaceHandler = () => {
-        dispatch(placesActions.addPlace(titleValue));
+        dispatch(placesActions.addPlace(titleValue, selectedImage));
         props.navigation.goBack();
     };
 
@@ -28,6 +34,7 @@ const NewPlaceScreen = props => {
                     onChangeText={titleChangeHandler} 
                     value={titleValue} 
                 />
+                <ImagePicker onImageTaken={imageTakenHandler} />
                 <Button title='Save Place' color={Colors.primary} onPress={savePlaceHandler} />
             </View>
         </ScrollView>
